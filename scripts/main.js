c951d3d5c33d80159bb57c3a4c1d6f86d51656c7
Alex Dudley <alexdudley@me.com>
2021-03-05 16:06:09 -0600

    finished up for the week.

--- scripts/main.js
+++
@@ -1,4 +0,0 @@
-console.log("too close for missiles, switching to guns.");
-
-import { EntryListComponent } from "./JournalEntryList.js";
-EntryListComponent();
---
+++ src/scripts/main.js
@@ -0,0 +1,4 @@
+console.log("too close for missiles, switching to guns.");
+
+import { getEntries } from "./data/DataManager.js";
+getEntries();
