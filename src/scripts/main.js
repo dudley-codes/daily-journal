76b921ffadcfe22f0b26422d954620faadd4e41b
Alex Dudley <alexdudley@me.com>
2021-03-10 15:18:53 -0600

    Add: journal entries can now be sorted by date or mood

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,5 +1,8 @@
 import { showPostList } from "./data/DataManager.js"
 import { eventListener } from "./EventListener.js"
+import { filterByDate, filterByMood } from "./FilterPosts.js"
 
 eventListener();
 showPostList();
+filterByDate();
+filterByMood();
