a849572151709dd9e76b61c5c102395217283f98
Alex Dudley <alexdudley@me.com>
2021-03-11 14:27:37 -0600

    added comment to main.js

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,10 @@
+//     _            _ _                             _
+//  __| |_    _  __| | |  ___ _   _   ___ ___    __| | ___  ___
+// /  _` | | | |/ _` | |/ _ \ | | |  / __/ _ \ / _`  |/ _ \/ __|
+// | (_| | |_| | (_| | |  __/ |_| | | (_| (_) | (_|  |  __/\__ \
+//  \__,_|\__,_|\__,_|_|\___|\__, |  \___\___/ \__,_ |\___||___/
+//                            |___/
+
 import { showPostList } from "./data/DataManager.js"
 import { eventListener } from "./EventListener.js"
 import { filterByDate, filterByMood } from "./FilterPosts.js"
